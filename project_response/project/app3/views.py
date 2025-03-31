bc8ddb21a2225ff87e7b728cc6541142834f9e69
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-03-31 13:34:59 +0530

    all

--- project_response/project/app3/views.py
+++ project_response/project/app3/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def app3(request):
+    return HttpResponse('<h1 style=color:blue><s>This is django class</s></h1>')
+    # first="""Hello This is 
+    # django Class"""
+    # return HttpResponse(first)
--- project_response/project/app4/views.py
+++ project_response/project/app4/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+def app4(request):
+    data= {'name': 'Himanshu', 'age':20, 'quali':"Btech"}
+    data= {'name': True, 'age':False, 'quali':None}
+    return JsonResponse(data)
