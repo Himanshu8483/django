2f39cc4b0dd96cdb84f8a648efca361c5109f589
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-04-29 13:31:16 +0530

    all

---
+++ model_relationship/project/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Aadhar(models.Model):
+    aadhar_no = models.IntegerField(unique = True)
+    created_by =  models.CharField(max_length=50)
+    alloted_date =  models.DateTimeField()
+    def __str__(self):
+        return str(self.aadhar_no)
+
+class Citizen(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    contact = models.IntegerField()
+    aadhar_no = models.OneToOneField(Aadhar, on_delete=models.PROTECT)
+    # , to_field='aadhar', related_name='student')
+    def __str__(self):
+        return str(self.name)
