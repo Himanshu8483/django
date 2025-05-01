ac60bcbca3ada530efbbebc855e4c2a4b14f8e60
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-05-01 13:52:51 +0530

    all

--- model_relationship/project/app/models.py
+++ model_relationship/project/app/models.py
@@ -6,12 +6,12 @@ class Aadhar(models.Model):
     created_by =  models.CharField(max_length=50)
     alloted_date =  models.DateTimeField()
     def __str__(self):
-        return str(self.aadhar_no)
+        return str(self.aadhar_no)  # object form return if comment str function(magic function)
 
 class Citizen(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, unique=True)
+    email = models.EmailField(max_length=254,unique=True)
     contact = models.IntegerField()
-    aadhar_no = models.OneToOneField(Aadhar, on_delete=models.PROTECT, to_field='aadhar_no')
+    aadhar_no = models.OneToOneField(Aadhar, on_delete=models.CASCADE, to_field='aadhar_no', related_name = "xyz") 
     def __str__(self):
         return self.name+' '+str(self.aadhar_no)
--- model_relationship/project/app/views.py
+++ model_relationship/project/app/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 def aadhar(request):
     data = Aadhar.objects.all()
-    # print (data)
+    # print (data)      
     # print (data.values())     # return in list form and only ther are values not keys
     print (data.values_list())
+    
+    data = Aadhar.objects.get(aadhar_no = 1234)
+    print(data.created_by)
+    print(data.alloted_date)
+    x = data.xyz    # for reverse accessing
+    print(x.name)
+    print(x.email)
+    print(x.contact)
+    print(x.aadhar_no)
+    
 def citizen(request):
-    data = Aadhar.objects.all()
-    print (data)
+    data = Citizen.objects.all()
+    print(data)
+    print(data.values())
+    print (data.values_list())  
+    # data = Citizen.objects.get(name = 'him')
+    data = Citizen.objects.get(id=1)
+    print(data.name)
+    print(data.email)
+    print(data.contact)
+    
+    print(data.aadhar_no)
+    
+    x = data.aadhar_no  # for forward accessing
+    print(x.aadhar_no)
+    print(x.created_by)
+    print(x.alloted_date)
