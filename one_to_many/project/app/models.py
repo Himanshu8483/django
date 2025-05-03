abd76ce226ac892b8ee2a57c66080b677650a90c
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-05-03 20:04:05 +0530

    all

--- one_to_many/project/app/models.py
+++ one_to_many/project/app/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
-# Create your models here.
 class Department(models.Model):
-    dep_name = models.CharField(unique = True)
-    dep_dis =  models.CharField(max_length=100)
-    dep_hod =  models.CharField(max_length=20)
+    dep_name = models.CharField(unique=True)  # Must be unique if used in to_field
+    dep_dis = models.CharField(max_length=100)
+    dep_hod = models.CharField(max_length=20)
+
     def __str__(self):
-        return str(self.dep_name)
+        return self.dep_name
 
 class Student(models.Model):
     stu_name = models.CharField(max_length=50)
-    stu_email = models.EmailField(max_length=254,unique=True)
+    stu_email = models.EmailField(unique=True)
     stu_contact = models.IntegerField()
-    stu_dep = models.ForeignKey(Department, on_delete=models.PROTECT) 
+    stu_dep = models.ForeignKey(
+        Department,
+        to_field='dep_name',  # Reference by dep_name instead of id
+        on_delete=models.PROTECT,
+        related_name='students'
+    )
+
     def __str__(self):
-        return self.stu_name+' '+str(self.stu_email)
+        return f"{self.stu_name} - {self.stu_dep.dep_name}"
--- one_to_many/project/app/views.py
+++ one_to_many/project/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from .models import Student, Department
 # Create your views here.
+def student(request):
+    all_data = Student.objects.all()
+    print(all_data)
+    print(all_data.values())
+    print(all_data.values_list())
+    
+    data = Student.objects.get(stu_name = 'Him')
+    print(data.stu_name)
+    print(data.stu_email)
+    print(data.stu_contact)
+    print(data.stu_dep.dep_name)
+    print(data.stu_dep.dep_dis)
+    print(data.stu_dep.dep_hod)
+    
+def department(request):
+    all_data = Department.objects.all()
+    print(all_data)
+    print(all_data.values())
+    print(all_data.values_list())
+    
+    data = Department.objects.get(dep_name='Data Analyst')
+    print(data.students)        # it return None
+    all_student = data.students.all()
+    print(all_student)
