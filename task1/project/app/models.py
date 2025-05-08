36ef46f4540e31dcce4814428c86fb167d55f1d4
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-05-08 12:16:08 +0530

    all

--- task1/project/app/models.py
+++ task1/project/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    s_no = models.IntegerField()
+    # s_no = models.IntegerField(default=1)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     contact = models.IntegerField()
