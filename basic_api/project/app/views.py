c03cabf310ca5e9f54c3640a056e9ef98a1e1984
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-05-26 22:30:57 +0530

    all

--- basic_api/project/app/views.py
+++ basic_api/project/app/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-# Create your views here.
+
 def student_list(request):
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
+    
+    # return JsonResponse(serializer.data, safe=False) 
+# or
     json_data = JSONRenderer().render(serializer.data)
     return HttpResponse(json_data, content_type='application/json')
+
 def student_detail(request, pk):
     user = Student.objects.get(pk=pk)
     serializer = StudentSerializer(user)
