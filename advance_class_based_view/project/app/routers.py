f0a25e47ed20910ba9cda3b82977c6e44fe6c148
Himanshu8483 <himanshukushwahabkp@gmail.com>
2025-06-03 13:48:42 +0530

    all

--- advance_class_based_view/project/app/routers.py
+++ advance_class_based_view/project/app/routers.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from .views import StudentViewSet
 router = routers.SimpleRouter()
 router.register(r'student', StudentViewSet, basename='student')
-urlpatterns = router.urls
+# urlpatterns = router.urls
