73a91d23d749e5c40f11353535d974f8341d4f8d
Himanshu8483 <himanshukushwahabkp@gmail.com>
2025-06-12 14:15:32 +0530

    all

--- simple_jwt/project/app/views.py
+++ simple_jwt/project/app/views.py
@@ -1,48 +1,48 @@
-# from django.shortcuts import render
-# from app.models import Student, HOD, Professor, Worker
-# from app.serializers import StudentSerializer, HODSerializer, ProfessorSerializer, WorkerSerializer
-# from rest_framework import viewsets
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
+from django.shortcuts import render
+from app.models import Student, HOD, Professor, Worker
+from app.serializers import StudentSerializer, HODSerializer, ProfessorSerializer, WorkerSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 
-# # Create your views here.
+# Create your views here.
 
-# class StudentViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAdminUser]
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
+class StudentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
     
-# class ProfessorViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-#     queryset = Professor.objects.all()
-#     serializer_class = ProfessorSerializer
+class ProfessorViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
     
-# class HODViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAuthenticatedOrReadOnly]
-#     queryset = HOD.objects.all()
-#     serializer_class = HODSerializer
+class HODViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = HOD.objects.all()
+    serializer_class = HODSerializer
     
-# class WorkerViewSet(viewsets.ModelViewSet):
-#     permission_classes = [AllowAny]
-#     queryset = Worker.objects.all()
-#     serializer_class = WorkerSerializer
+class WorkerViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = Worker.objects.all()
+    serializer_class = WorkerSerializer
     
     
     
 
-# from rest_framework.authtoken.views import ObtainAuthToken
-# from rest_framework.authtoken.models import Token
-# from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# class CustomAuthToken(ObtainAuthToken):
+class CustomAuthToken(ObtainAuthToken):
 
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.serializer_class(data=request.data, context={'request': request})
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data['user']
-#         token, created = Token.objects.get_or_create(user=user)
-#         return Response({
-#             'token': token.key,
-#             'user_id': user.pk,
-#             'email': user.email,
-#             'username': user.username
-#         })
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'username': user.username
+        })
