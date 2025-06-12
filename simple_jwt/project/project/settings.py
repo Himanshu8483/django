80995f666fbeb1f1e107a074483f943f934f052d
Himanshu8483 <himanshukushwahabkp@gmail.com>
2025-06-12 14:14:29 +0530

    all

--- simple_jwt/project/project/settings.py
+++ simple_jwt/project/project/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,      # for new token after refresh
     "BLACKLIST_AFTER_ROTATION": False,
